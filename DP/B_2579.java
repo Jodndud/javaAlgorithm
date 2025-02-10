143dcd1d64aca9f9d06916ca5dd9461920d29cf5
jodndud <doriconi@gmail.com>
2025-02-11 01:26:52 +0900

    2/11

--- DP/B_2579.java
+++ DP/B_2579.java
@@ -3,28 +3,30 @@ package wooyeong.DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class B_2579 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-
-            int N = Integer.parseInt(br.readLine());
-            int[] dp = new int[12];
-
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-            for (int j = 4; j < N+1; j++) {
-                for (int k = 1; k < 4; k++) {
-                    dp[j] += dp[j-k];
-                }
-            }
-
-            System.out.println(dp[N]);
+        int[] dp = new int[301];
+        int[] stair = new int[T+1];
+        for (int i = 1; i <= T; i++) {
+            stair[i] = Integer.parseInt(br.readLine());
         }
+
+        dp[1] = stair[1];
+        dp[2] = stair[1] + stair[2];
+        dp[3] = Math.max(dp[2], dp[1]+stair[3]);
+        for (int i=4; i<=T; i++) {
+            if(i==T){
+                dp[i] = Math.max(dp[i-2]+stair[i], dp[i-3]+stair[i-1]+stair[i]);
+            }
+            dp[i] = Math.max(Math.max(dp[i-1], dp[i-2]+stair[i]), dp[i-3]+stair[i-1]+stair[i]);
+        }
+
+        System.out.println(dp[T]);
 
         br.close();
     }
