f48e20cbeceb0e953d8909ed5b6d99143a644477
Jodndud <doriconi@gmail.com>
2025-07-11 17:39:57 +0900

    b2606

---
+++ Algorythm/src/bfs_dfs/b2606.java
@@ -0,0 +1,27 @@
+package bfs_dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+public class b2606 {
+
+    static int n;
+    static int node;
+    static List<int []> arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        node = Integer.parseInt(br.readLine());
+        arr = new LinkedList<>();
+
+        for(int i=0;i<node;i++){
+
+        }
+    }
+
+}
