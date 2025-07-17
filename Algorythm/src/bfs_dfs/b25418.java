7a1dc828c2a7097f6d9d8e2a42c76aa2a59fcccf
wooyeong <doriconi@gmail.com>
2025-07-17 22:11:45 +0900

    b2178

--- Algorythm/src/bfs_dfs/b25418.java
+++ Algorythm/src/bfs_dfs/b25418.java
@@ -1,52 +1,45 @@
 package bfs_dfs;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class b25418 {
-
-    static int A;
-    static int K;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
-        A = Integer.parseInt(input[0]);
-        K = Integer.parseInt(input[1]);
+        int A = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
 
         visited = new boolean[K+1];
-
-        System.out.println(bfs(A));
+        System.out.println(bfs(A, K));
     }
 
-    private static int bfs(int start) {
+    private static int bfs(int A, int K) {
         Queue<int[]> q = new LinkedList<>();
 
-        q.add(new int[]{start, 0});
-        visited[start] = true;
+        q.add(new int[]{A, 0});
+        visited[A] = true;
 
         while(!q.isEmpty()){
-            int[] next = q.poll();
-            int value = next[0];
-            int step = next[1];
+            int[] current = q.poll();
+            int next = current[0];
+            int count = current[1];
 
-            // 종료조건
-            if(value == K){
-                return step;
+            if(next==K){
+                return count;
             }
 
-            // 연산1
-            if(value + 1 <=K && !visited[value+1]){
-                visited[value+1] = true;
-                q.add(new int[]{value+1, step+1});
+            if(next+1<=K && !visited[next+1]){
+                visited[next+1] = true;
+                q.add(new int[]{next+1, count+1});
             }
-            // 연산2
-            if(next[0]*2 <=K && !visited[value*2]){
-                visited[value*2] = true;
-                q.add(new int[]{value*2, step+1});
+
+            if(next*2<=K && !visited[next*2]){
+                visited[next*2] = true;
+                q.add(new int[]{next*2, count+1});
             }
         }
         return -1;
