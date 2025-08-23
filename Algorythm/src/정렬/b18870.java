1e16287064d5c6a3a56e482ca7be8cb419e528dd
jodndud <doriconi@gmail.com>
2025-08-23 23:58:37 +0900

    sort

--- Algorythm/src/정렬/b18870.java
+++ Algorythm/src/정렬/b18870.java
@@ -11,23 +11,27 @@ public class b18870 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        HashMap<Integer, Integer> dic = new HashMap<>();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] array = new int[N];
+        int[] sortArray = new int[N];
         for(int i=0;i<N;i++){
             array[i] = Integer.parseInt(st.nextToken());
-            dic.put(array[i], 0); // value 0으로 초기화
+            sortArray[i] = array[i];
         }
 
-        // 복제 배열 생성
-        int[] sorted = array.clone();
+        Arrays.sort(sortArray);
+        System.out.println(Arrays.toString(sortArray));
 
-        // 복제해서 Set으로 변환
-        Arrays.sort(sorted);
-
-        for(int i=0;i<N;i++){
-            System.out.print(array[i] + " ");
+        HashMap<Integer, Integer> rankMap = new HashMap<>();
+        int rank = 0;
+        for(int value : sortArray){
+            if(!rankMap.containsKey(value)){
+                rankMap.put(value, rank++);
+            }
         }
+
+
+
+
     }
 }
