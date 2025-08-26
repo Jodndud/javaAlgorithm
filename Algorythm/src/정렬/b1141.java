258554bc9060bb7ba3f8d47f7e12be9ce8fbea3d
jodndud <doriconi@gmail.com>
2025-08-27 00:40:45 +0900

    sort

--- Algorythm/src/정렬/b1141.java
+++ Algorythm/src/정렬/b1141.java
@@ -4,40 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class b1141 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         ArrayList<String> arr = new ArrayList<>();
-        // 입력 받은 배열(기본)
         for(int i=0;i<n;i++){
             arr.add(br.readLine());
         }
 
-        int count=0;
-        // 부분집합
-        // 백트래킹으로 구현
+        System.out.println(arr);
+        Collections.sort(arr, (a, b) -> b.length() - a.length());
 
+        ArrayList<String> answer = new ArrayList<>();
 
-        // 비트연산자
-        for(int mask=0;mask<(1<<n);mask++){
-            ArrayList<String> result = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) { // i번째 비트가 1이면 포함
-                    result.add(arr.get(i));
+        for (String s : arr) {
+            boolean isPrefix = false;
+            for (String t : answer) {
+                if (t.startsWith(s)) { // s가 t의 접두사라면 제외
+                    isPrefix = true;
+                    break;
                 }
             }
-
-            // 속하는지 판단
-            for(int i=0;i<result.size();i++){
-                if(){
-                    count++;
-                }
+            if (!isPrefix) {
+                answer.add(s);
             }
         }
+
+        System.out.println(answer.size());
     }
 }
